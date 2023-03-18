c106e977e20a0b60162c58f0da02508bf96b275a
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-03-18 14:32:47 +0100

    started file-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { LoginComponent } from './login/login.component';
 import { ListComponent } from './list/list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { FileListComponent } from './file-list/file-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ListComponent,
+    FileListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/file-list/file-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-file-list',
+  templateUrl: './file-list.component.html',
+  styleUrls: ['./file-list.component.scss']
+})
+export class FileListComponent {
+  @Input() files:gapi.client.drive.File[] = [];
+}
