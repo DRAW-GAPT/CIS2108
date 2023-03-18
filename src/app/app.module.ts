45cdc7ecffabf00db6939f7b66c258a63511995d
David Briffa <david.briffa.21@um.edu.mt>
2023-03-18 19:04:43 +0100

    background image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ListComponent } from './list/list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table'  
 import { FileListComponent } from './file-list/file-list.component';
-import {MatTableModule} from '@angular/material/table'
+
 
 @NgModule({
   declarations: [
