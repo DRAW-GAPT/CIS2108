ce662efdbd2118ca51d8dcfb35d74b24e956c5ac
David Briffa <david.briffa.21@um.edu.mt>
2023-03-19 13:03:04 +0100

    cookie store on login

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { GoogleAPIService } from '../google-api.service';
 
 @Component({
@@ -8,12 +9,18 @@ import { GoogleAPIService } from '../google-api.service';
 })
 export class ListComponent {
 
-
   //todo this is a temp class - need to redo with proper async
   list:gapi.client.drive.File[] = [];
 
-  constructor(public googleAPIService: GoogleAPIService){
+  constructor(public googleAPIService: GoogleAPIService,  private cookie:CookieService){
     this.init();
+    
+    if(!this.cookie.get("googleAuthToken")){
+      const expiryDate = new Date();
+      expiryDate.setDate(expiryDate.getDate() + 1);
+      this.cookie.set('googleAuthToken', gapi.client.getToken().access_token, expiryDate, '/');
+    }
+
   }
 
   async init() {
