aa38268c5f78bc8e789d21c57b43758c7b9b4ff8
Rianne Azzopardi <rianneazzopardi@gmail.com>
2023-03-21 14:05:59 +0100

    added the other filters

--- src/app/filter-chips/filter-chips.component.ts
+++ src/app/filter-chips/filter-chips.component.ts
@@ -11,11 +11,26 @@ export class FilterChipsComponent {
   searchText!: string;
   // SET TO AN ARRAY OF OWNERS IN THE TABLE
   owners = ['Andrea Borg', 'Rianne Azzopardi', 'David Briffa', 'Wayne Borg', 'Ema Grech'];
+  shared = ['None','Andrea Borg', 'Rianne Azzopardi', 'David Briffa', 'Wayne Borg'];
+  types = ['.pdf','.jpg','.doc','.png'];
+  filteredType = this.types;
   filteredOptions = this.owners;
+  filteredShared = this.shared;
 
   filterOptions() {
     this.filteredOptions = this.owners.filter(owner => {
       return owner.toLowerCase().includes(this.searchText.toLowerCase());
+    });
+  }
+  filterType() {
+    this.filteredType = this.types.filter(type => {
+      return type.toLowerCase().includes(this.searchText.toLowerCase());
+    });
+  }
+
+  filterShared() {
+    this.filteredShared = this.shared.filter(share => {
+      return share.toLowerCase().includes(this.searchText.toLowerCase());
     });
   }
 }
