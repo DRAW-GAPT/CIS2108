b2913cca55b9ffe18ac19079e4f46643c1adc1fb
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-03-22 00:01:17 +0100

    getOwnersList

--- src/app/filter-chips/filter-chips.component.ts
+++ src/app/filter-chips/filter-chips.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import { Component, Input, Output } from '@angular/core';
+import { distinct } from '../util';
+
 
 
 @Component({
@@ -8,9 +9,13 @@ import {FormGroup, FormControl} from '@angular/forms';
   styleUrls: ['./filter-chips.component.scss']
 })
 export class FilterChipsComponent {
+
+  @Input() unfilteredFiles:gapi.client.drive.File[] = [];
+  @Output() filteredFiles:gapi.client.drive.File[] = [];
+
   searchText!: string;
   // SET TO AN ARRAY OF OWNERS IN THE TABLE
-  owners = ['Andrea Borg', 'Rianne Azzopardi', 'David Briffa', 'Wayne Borg', 'Ema Grech'];
+  owners = this.getOwnersList();
   shared = ['None','Andrea Borg', 'Rianne Azzopardi', 'David Briffa', 'Wayne Borg'];
   types = ['.pdf','.jpg','.doc','.png'];
   filteredType = this.types;
@@ -18,9 +23,9 @@ export class FilterChipsComponent {
   filteredShared = this.shared;
 
   filterOptions() {
-    this.filteredOptions = this.owners.filter(owner => {
-      return owner.toLowerCase().includes(this.searchText.toLowerCase());
-    });
+    // this.filteredOptions = this.owners.filter(owner => {
+    //   return owner.toLowerCase().includes(this.searchText.toLowerCase());
+    // });
   }
   filterType() {
     this.filteredType = this.types.filter(type => {
@@ -33,6 +38,24 @@ export class FilterChipsComponent {
       return share.toLowerCase().includes(this.searchText.toLowerCase());
     });
   }
+
+  
+  getOwnersList() {
+
+    console.log("getting owners");
+
+    let ownersList:gapi.client.drive.User[] = this.unfilteredFiles
+      .map((file:gapi.client.drive.File)=>file.owners) // get owners array
+      .map((owners)=> (owners && owners[0]) ? owners[0]:null) //get 0th element (usually file only has 1 anyways) - or undefined if array is null or empty
+      .filter((owner): owner is gapi.client.drive.User => owner!==null && owner!==undefined); //remove nulls/undefineds
+      
+      ownersList = distinct(ownersList);
+
+      //todo return object isntead of string
+      return ownersList.map((o)=>o.emailAddress??"");
+
+  }
+
 }
 
 
