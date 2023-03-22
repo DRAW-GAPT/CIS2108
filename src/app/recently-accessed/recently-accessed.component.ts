29f2527a869304782495288d81a3effd3acab3e3
Rianne Azzopardi <rianneazzopardi@gmail.com>
2023-03-22 08:36:20 +0100

    optimized recently accessed cards

--- src/app/recently-accessed/recently-accessed.component.ts
+++ src/app/recently-accessed/recently-accessed.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recently-accessed.component.scss']
 })
 export class RecentlyAccessedComponent {
+  // documents = ['doggy.docx', 'apple.docx', 'wayne.png', 'spongebob.png'];
+  documents = [    { name: 'doggy.docx', owner: 'Rianne Azzopardi', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },    { name: 'apple.docx', owner: 'Andrea Borg', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },    { name: 'wayne.png', owner: 'David Briffa', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' }, { name: 'spongebob.png', owner: 'Wayne Borg', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' }   ];
 
 }
