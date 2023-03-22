3fdf2f4608bd13cffbba7ca8d63372bab7b4966d
= <=>
2023-03-22 16:05:12 +0100

    changed api to async

--- src/app/filter-chips/filter-chips.component.ts
+++ src/app/filter-chips/filter-chips.component.ts
@@ -10,12 +10,24 @@ import { distinct } from '../util';
 })
 export class FilterChipsComponent {
 
-  @Input() unfilteredFiles:gapi.client.drive.File[] = [];
+  _unfilteredFiles:gapi.client.drive.File[] = [];
+  get unfilteredFiles():gapi.client.drive.File[] {
+    return this._unfilteredFiles;
+  }
+
+  @Input() set unfilteredFiles(value:gapi.client.drive.File[]){
+    this._unfilteredFiles = [...value];
+    console.log(value);
+    this.updateDropdowns();
+  }
+
+
+
   @Output() filteredFiles:gapi.client.drive.File[] = [];
 
   searchText!: string;
   // SET TO AN ARRAY OF OWNERS IN THE TABLE
-  owners = this.getOwnersList();
+  owners:any[] = [];
   shared = ['None','Andrea Borg', 'Rianne Azzopardi', 'David Briffa', 'Wayne Borg'];
   types = ['.pdf','.jpg','.doc','.png'];
   filteredType = this.types;
@@ -23,9 +35,8 @@ export class FilterChipsComponent {
   filteredShared = this.shared;
 
   filterOptions() {
-    // this.filteredOptions = this.owners.filter(owner => {
-    //   return owner.toLowerCase().includes(this.searchText.toLowerCase());
-    // });
+    console.trace("filtering owners")
+    this.filteredOptions = this.owners;
   }
   filterType() {
     this.filteredType = this.types.filter(type => {
@@ -39,12 +50,17 @@ export class FilterChipsComponent {
     });
   }
 
+
+  updateDropdowns(){
+    this.owners = this.getOwnersList();
+  }
+
   
   getOwnersList() {
 
     console.log("getting owners");
 
-    let ownersList:gapi.client.drive.User[] = this.unfilteredFiles
+    let ownersList:gapi.client.drive.User[] = this._unfilteredFiles
       .map((file:gapi.client.drive.File)=>file.owners) // get owners array
       .map((owners)=> (owners && owners[0]) ? owners[0]:null) //get 0th element (usually file only has 1 anyways) - or undefined if array is null or empty
       .filter((owner): owner is gapi.client.drive.User => owner!==null && owner!==undefined); //remove nulls/undefineds
@@ -54,6 +70,10 @@ export class FilterChipsComponent {
       //todo return object isntead of string
       return ownersList.map((o)=>o.emailAddress??"");
 
+  }
+
+  ngAfterViewInit(){
+    this.updateDropdowns();
   }
 
 }
