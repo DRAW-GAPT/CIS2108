7b833cd7e8df5467fd8f0dcb607bc9a2903bb096
Rianne Azzopardi <rianneazzopardi@gmail.com>
2023-03-22 08:45:17 +0100

    fixed cards

--- src/app/recently-accessed/recently-accessed.component.ts
+++ src/app/recently-accessed/recently-accessed.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class RecentlyAccessedComponent {
   // documents = ['doggy.docx', 'apple.docx', 'wayne.png', 'spongebob.png'];
-  documents = [    { name: 'doggy.docx', owner: 'Rianne Azzopardi', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },    { name: 'apple.docx', owner: 'Andrea Borg', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },    { name: 'wayne.png', owner: 'David Briffa', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' }, { name: 'spongebob.png', owner: 'Wayne Borg', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' }   ];
+  documents = [    { name: 'doggy.docx', owner: 'Rianne Azzopardi', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },    { name: 'apple.docx', owner: 'Andrea Borg', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },    { name: 'wayne.png', owner: 'David Briffa', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' }, { name: 'spongebob.png', owner: 'Wayne Borg', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' }, { name: 'ema.docx', owner: 'Ema Grech', preview: 'https://material.angular.io/assets/img/examples/shiba2.jpg' } ];
 
 }
