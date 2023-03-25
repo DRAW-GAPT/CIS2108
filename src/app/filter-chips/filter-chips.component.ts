d94fff05dc580ec1ebf04970f1a4607123db127f
Andrea Borg <andreaborg217@gmail.com>
2023-03-25 20:05:56 +0100

    seperated the inputs for owner/shared for binding

--- src/app/filter-chips/filter-chips.component.ts
+++ src/app/filter-chips/filter-chips.component.ts
@@ -189,6 +189,114 @@ export class FilterChipsComponent {
 
   
 
+
+//new chips functionality
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  owners: String[] = [];
+  sharedWith: String[] = [];
+  permissionsSelected: String[] = [];
+
+  addOwner(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    this.permissionsSelected.push("reader");
+    // Add our fruit
+    if (value) {
+      this.owners.push(value);
+      console.log(this.filterByOwner());
+
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  removeOwner(owner: String): void {
+    const index = this.owners.indexOf(owner);
+
+    if (index >= 0) {
+      this.owners.splice(index, 1);
+    }
+  }
+
+  editOwner(owner: String, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.removeOwner(owner);
+      return;
+    }
+
+    // Edit existing owner
+    const index = this.owners.indexOf(owner);
+    if (index >= 0) {
+      this.owners[index] = value;
+    }
+  }
+
+  addShared(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    this.permissionsSelected.push("reader");
+    // Add our fruit
+    if (value) {
+      this.sharedWith.push(value);
+      console.log(this.filterByOwner());
+
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  removeShared(shared: String): void {
+    const index = this.sharedWith.indexOf(shared);
+
+    if (index >= 0) {
+      this.sharedWith.splice(index, 1);
+    }
+  }
+
+  editShared(shared: String, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    if (!value) {
+      this.removeShared(shared);
+      return;
+    }
+
+    const index = this.sharedWith.indexOf(shared);
+    if (index >= 0) {
+      this.sharedWith[index] = value;
+    }
+  }
+  
+  //filters the list by owner, called whenever a new email is added to the owner's chip
+  async filterByOwner(){
+    const searchQuery = `trashed=false and (${this.owners.map(owner => `'${owner}' in owners`).join(' or ')})`;
+    const response = await gapi.client.drive.files.list({
+      q: searchQuery,
+      fields: 'nextPageToken, files(id, name, createdTime, modifiedTime, owners,size, lastModifyingUser, iconLink, fileExtension, permissions)'
+    });
+    console.log(response.result);
+    this.owners = _.sortBy(this.owners,"displayName")
+
+    return this.owners;
+  }
+
+  //under construction
+  async filterByPermissions(){
+    const searchQuery = `trashed=false and (${this.permissionsSelected
+      .map(permissionsSelected=> `'${ permissionsSelected }' in permissions`).join(' or ')})`;
+    const response = await gapi.client.drive.files.list({
+      q: "'me' in owners and " + searchQuery,
+      fields: 'nextPageToken, files(id, name, createdTime, modifiedTime, owners,size, lastModifyingUser, iconLink, fileExtension, permissions)'
+    });
+    console.log(response.result);
+    this.owners = _.sortBy(this.owners,"displayName")
+
+    return this.owners;
+  }
   
   
 }
