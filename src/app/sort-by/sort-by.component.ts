f4780e892e9ea7a18276bff9b55a4fe78b6e5da3
Rianne Azzopardi <rianneazzopardi@gmail.com>
2023-04-05 16:36:20 +0200

    implemented a sort by container

---
+++ src/app/sort-by/sort-by.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-by',
+  templateUrl: './sort-by.component.html',
+  styleUrls: ['./sort-by.component.scss']
+})
+export class SortByComponent {
+
+}
