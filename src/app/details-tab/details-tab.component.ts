a42d3b5c43008226166e4e5627fa1568639b6359
Andrea Borg <andreaborg217@gmail.com>
2023-04-09 18:12:00 +0200

    details tab (layouts done, loc. & type not done)

--- src/app/details-tab/details-tab.component.ts
+++ src/app/details-tab/details-tab.component.ts
@@ -7,4 +7,18 @@ import { Component, Input } from '@angular/core';
 })
 export class DetailsTabComponent {
   @Input() file: any;
+
+  formatCreated(date: Date): string{
+    let options: any = { day: "numeric", month: "long", year: "numeric", hour: "numeric", minute: "numeric"};
+    return new Date(date).toLocaleDateString(undefined, options);
+  }
+
+  formatLastModified = (date: Date, user:any): string => {
+    user = user?.displayName;
+    let options: any = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    let dateTime = new Date(date).toLocaleDateString(undefined, options);
+    
+    if (user) return dateTime + " by " + user;
+    else return dateTime
+  }
 }
--- src/app/item-tabs/item-tabs.component.ts
+++ src/app/item-tabs/item-tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-item-tabs',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-tabs.component.scss']
 })
 export class ItemTabsComponent {
+  @Input() file: any;
   showTabs : boolean = false;
 }
