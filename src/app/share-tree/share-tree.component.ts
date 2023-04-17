e42e7002cbfc1c384a5ca032511a15d736b4c433
Rianne Azzopardi <rianneazzopardi@gmail.com>
2023-04-17 12:31:41 +0200

    edited the share tree

--- src/app/share-tree/share-tree.component.ts
+++ src/app/share-tree/share-tree.component.ts
@@ -19,26 +19,38 @@ export class ShareTreeComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     // create an array with nodes
     const nodes = new DataSet<any>([
-      { id: 1, label: 'Node 1' },
-      { id: 2, label: 'Node 2' },
-      { id: 3, label: 'Node 3' },
-      { id: 4, label: 'Node 4' },
-      { id: 5, label: 'Node 5' },
+      { id: 1, label: 'Rianne Azzopardi\nOwner', image: 'https://www.hartz.com/wp-content/uploads/2022/04/small-dog-owners-1.jpg' },
+      { id: 2, label: 'David Briffa\nEditor', image: 'https://lh3.googleusercontent.com/a-/ACB-R5RxStIkJMNjIGOm84bx6y3QhwGMPCh0e01HUWDwBto=s64' },
+      { id: 3, label: 'Andrea Borg\nEditor', image: 'https://lh3.googleusercontent.com/a-/ACB-R5RxStIkJMNjIGOm84bx6y3QhwGMPCh0e01HUWDwBto=s64' },
+      { id: 4, label: 'Wayne Borg\nEditor', image: 'https://lh3.googleusercontent.com/a-/ACB-R5RxStIkJMNjIGOm84bx6y3QhwGMPCh0e01HUWDwBto=s64' },
+      { id: 5, label: 'Peter Xuereb\nViewer', image: 'https://lh3.googleusercontent.com/a-/ACB-R5RxStIkJMNjIGOm84bx6y3QhwGMPCh0e01HUWDwBto=s64' },
+      { id: 6, label: 'Michel Camilleri\nViewer',image: 'https://lh3.googleusercontent.com/a-/ACB-R5RxStIkJMNjIGOm84bx6y3QhwGMPCh0e01HUWDwBto=s64' },
     ]);
  
     // create an array with edges
     const edges = new DataSet<any>([
+      { from: '1', to: '5' },
+      { from: '1', to: '6' },
       { from: '1', to: '3' },
-      { from: '1', to: '2' },
-      { from: '2', to: '4' },
-      { from: '2', to: '5' },
+      { from: '3', to: '4' },
+      { from: '3', to: '2' },
     ]);
+
+    const options = {
+      directed: true,
+                  nodes: {
+                      shape: 'circularImage',
+                      image: 'image',
+                      label: 'Label'
+                  }
+      };
  
     const data = { nodes, edges };
  
     const container = this.visNetwork;
-    this.networkInstance = new Network(container.nativeElement, data, {});
+    this.networkInstance = new Network(container.nativeElement, data, options);
   }
 }
+
 
 
