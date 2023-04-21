6cca4bc02bb13b8ac9bad18f8cea8fd76cd8174c
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-04-21 09:39:27 +0200

    deleting files for gh-pages

--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    googleAPIKey: "AIzaSyBijWgfUPGkxFlA-elNrsUOpykVS6CZHCY",
-    googleClientID: "904348340648-llv73mmav90iatvpkegg1hnbptet28v7.apps.googleusercontent.com"
-};
